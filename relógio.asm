7ec7f30452ba1dba4347c252051b13897489103c
Rogério Gabriel <153862792+gccarvalho1@users.noreply.github.com>
2023-12-14 17:10:25 -0300

    Update and rename Relógio_Rogério.asm to relógio.asm

--- relógio.asm
+++ relógio.asm
@@ -16,10 +16,10 @@ u_min 		res 1 ; ====> Variavel de unidades de minutos no relogio
 d_min 		res 1 ; ====> Variavel de dezena de minutos no relogio
 u_hor		res 1 ; ====> Variavel de unidade de hora no relogio
 d_hor 		res 1 ; ====> Variavel de dezena de hora no relogio
-displ		res 1 ; ====> Variavel do relÛgio : MM:SS e HH:MM
-w_temp		res 1 ; ====> Variavel de restauraÁ„o de contexto na interrupcao
-s_temp 		res 1 ; ====> Variavel de restauraÁ„o de contexto na interrupcao
-;cont   		res 1 ; ====> Variavel dos segundos no relÛgio	
+displ		res 1 ; ====> Variavel do rel√≥gio : MM:SS e HH:MM
+w_temp		res 1 ; ====> Variavel de restaura√ß√£o de contexto na interrupcao
+s_temp 		res 1 ; ====> Variavel de restaura√ß√£o de contexto na interrupcao
+;cont   		res 1 ; ====> Variavel dos segundos no rel√≥gio	
 flags		res 1 ; ====> Variavel de pisca dos pontos no relogio
 incre		res 1 ; ====> Variavel que faz a incrementacao entrar no modo ajuste
 d_alarmeH	res 1 ; ====> Variavel de dezena de hora no relogio: =======================================================
@@ -32,13 +32,13 @@ aux			res 1 ; ====> Variavel que liga e desliga alarme
 
 
 inicio    
-	;========== DeclaraÁ„o do sistema ==============;
+	;========== Declara√ß√£o do sistema ==============;
 	banksel 	TRISD
 	movlw 		00000000b
 	movwf 		TRISD
-	movlw		00000000b ;0 - saÌda 1- entrada
+	movlw		00000000b ;0 - sa√≠da 1- entrada
 	movwf		TRISA
-	movlw		00111111b ;0 - saÌda 1- entrada
+	movlw		00111111b ;0 - sa√≠da 1- entrada
 	movwf		TRISB
 	movlw		6
 	movwf		ADCON1
@@ -49,7 +49,7 @@ inicio
 	clrf		PORTD
 	clrf		PORTA
 	clrf		PORTB
-; ==================== Limpeza de vari·veis=========
+; ==================== Limpeza de vari√°veis=========
 	clrf		u_seg
 	clrf		d_seg
 	clrf 		u_min
@@ -97,7 +97,7 @@ loop
 
 
 	pageselw	ajustar
-	call 		ajustar	; =======>: Funcao: Ajustar RelÛgio
+	call 		ajustar	; =======>: Funcao: Ajustar Rel√≥gio
 	clrf		PCLATH
 
 	pageselw	ajustarAlarme
@@ -114,7 +114,7 @@ loop
 
 bcd_7seg	
 
-	;============> DefiniÁ„o do display
+	;============> Defini√ß√£o do display
 	addwf PCL,f
 	retlw	00111111b;0
 	retlw	00000110b;1
@@ -163,13 +163,13 @@ seg
 	clrf	d_min
 	incf	u_hor,f
 
-	movf	d_hor,w 	;Teste se D_HOR È igual 2
+	movf	d_hor,w 	;Teste se D_HOR √© igual 2
 	xorlw	2
-	btfss	STATUS,Z	; Se dezena nao for 2, n„o skip e vai pro goto
+	btfss	STATUS,Z	; Se dezena nao for 2, n√£o skip e vai pro goto
 	goto	$ + 7
 
 	movf	u_hor,w
-	xorlw	4			; Testa de a U_HOR È igual a 4
+	xorlw	4			; Testa de a U_HOR √© igual a 4
 	btfss	STATUS,Z	; Vai pra incremento da unidade
 	goto	$+3
 	clrf	u_hor
@@ -225,7 +225,7 @@ inter
 	retfie
 	
 rell
-	; ===========> RelÛgio de Minutos e Segundos MM:SS;
+	; ===========> Rel√≥gio de Minutos e Segundos MM:SS;
 	clrc
 					;Obs: O delay2 chamado a cada instante e para fazer a ilusao de todos os display's piscarem ao mesmo instante.
 	bcf 	PORTA,5
@@ -274,7 +274,7 @@ rell
 	return
 	org	100h
 rell2
-	;========> RelÛgio de Horas e minutos;
+	;========> Rel√≥gio de Horas e minutos;
 	clrc
 
 	bcf 	PORTA,5
@@ -345,10 +345,10 @@ delay2 	;================DELAY 0.5s==========
 	goto	$ - 8
 
 	return
-define ; ========= Essa funÁ„o define o que vai ser mostrado no  display ====;
+define ; ========= Essa fun√ß√£o define o que vai ser mostrado no  display ====;
 
 	movf 	incre,w
-	xorlw	0			;=====> Se a vari·vel de modo ajuste for diferente de zero vai para o "call goto"
+	xorlw	0			;=====> Se a vari√°vel de modo ajuste for diferente de zero vai para o "call goto"
 	btfsc	STATUS,Z
 	goto	$+2
 	goto 	ajuste
@@ -370,7 +370,7 @@ define ; ========= Essa funÁ„o define o que vai ser mostrado no  display ====;
 
 
 botao
-; ======== Ajuste da funÁ„o do display
+; ======== Ajuste da fun√ß√£o do display
 
 	btfsc 	PORTB,0
 	goto	$ + 8
@@ -378,7 +378,7 @@ botao
 	goto	$-1
 	incf	incre,f
 	movf	incre,w
-	xorlw	9		;======= Quando a variavel incre for 9 vai limpar a vari·vel incre
+	xorlw	9		;======= Quando a variavel incre for 9 vai limpar a vari√°vel incre
 	btfsc	STATUS,Z
 	clrf	incre
 
@@ -399,7 +399,7 @@ botao
 	btfss	PORTB,5
 	goto	$-1
 	xorwf	aux,f
-	movlw	00000010b;========== AtivaÁ„o do ponto no display
+	movlw	00000010b;========== Ativa√ß√£o do ponto no display
 	xorwf	flags,f
 	
 	
@@ -415,7 +415,7 @@ botao
 	
 ajuste
 	
-;======= Nessa parte temos a configuraÁ„o das horas e minutos do relogio modo ajuste======;
+;======= Nessa parte temos a configura√ß√£o das horas e minutos do relogio modo ajuste======;
 	bcf			PORTA,5
 	bcf			PORTA,4
 	bcf			PORTA,3
@@ -469,7 +469,7 @@ ajuste
 	call		bcd_7seg
 	movwf		PORTD
 	bsf			PORTA,5
-;====== Nessa parte È o ajuste do alarme a ser configurado. =====
+;====== Nessa parte √© o ajuste do alarme a ser configurado. =====
 	movf		incre,w
 	xorlw		5		;========== Quando a variavel incre for 5
 	btfss		STATUS,Z
@@ -524,12 +524,12 @@ ajuste
 	return
 	
 	org 200h
-ajustar   ;	Essa funÁ„o que realmente altera os valores:
+ajustar   ;	Essa fun√ß√£o que realmente altera os valores:
 	clrc
 	; Incrementa na dezena de horas
 	movf		incre,w
 	xorlw		1
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		@3		
 	btfsc		PORTB,2		
 	goto		@3
@@ -537,11 +537,11 @@ ajustar   ;	Essa funÁ„o que realmente altera os valores:
 	goto		$-1
 
 	movf		d_hor,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$+2
 	goto		@3
-	movf		u_hor,w		; Nessa parte È a configuracao para nao deixar colocar o valor diferente de 23 no display.
+	movf		u_hor,w		; Nessa parte √© a configuracao para nao deixar colocar o valor diferente de 23 no display.
 	xorlw		3			; Faz um teste em cada variavel para caso a dezena de horas for 2, a unidade de hora nao pode ter nenhum valor 
 	btfsc		STATUS,Z	; acima de 3, caso tenha, zera a varivel de unidade de hora e incrementa, caso apertado, na variavel de dezena
 	goto 		@H
@@ -563,111 +563,111 @@ ajustar   ;	Essa funÁ„o que realmente altera os valores:
 	; Incrementa na unidade de horas
 @3	movf		incre,w
 	xorlw		2
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+17			
 	btfsc		PORTB,2		
 	goto		$+15
 	btfss		PORTB,2
 	goto		$-1
 	movf		d_hor,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$ + 5
 	movf		u_hor,w
 	xorlw		3			; Se a unidade de hora for 3 ele vai pro goto e nao incrementa
 	btfsc		STATUS,Z
 	goto		$ + 5
 	movf		u_hor,w
-	xorlw		9			; N„o deixa passar de 9
+	xorlw		9			; N√£o deixa passar de 9
 	btfss		STATUS,Z
 	incf		u_hor,f
 	
 	; Incrementa na dezena de minutos
 	movf		incre,w
 	xorlw		3
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,2		
 	goto		$+7
 	btfss		PORTB,2
 	goto		$-1
 	movf		d_min,w		
-	xorlw		5			; Faz o teste para n„o exceder o valor 9 em dezenas de minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		5			; Faz o teste para n√£o exceder o valor 9 em dezenas de minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	incf		d_min,f
 
 	; Incrementa na unidade de minutos
 	movf		incre,w
 	xorlw		4
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,2		
 	goto		$+7
 	btfss		PORTB,2
 	goto		$-1
 	movf		u_min,w		
-	xorlw		9			; Faz o teste para n„o exceder o valor 9 unidade dm minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		9			; Faz o teste para n√£o exceder o valor 9 unidade dm minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	incf		u_min,f
 	; Decremento na dezena de horas
 	movf		incre,w
 	xorlw		1
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_hor,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 0 de unidade de hora
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 0 de unidade de hora
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		d_hor,f
 	; Decremento na unidade de horas
 	movf		incre,w
 	xorlw		2
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+17			
 	btfsc		PORTB,1		
 	goto		$+15
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_hor,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$ + 5
 	movf		u_hor,w
 	xorlw		0			; Se a unidade de hora for diferente 0 ele vai pro goto
 	btfsc		STATUS,Z
 	goto		$ + 5
 	movf		u_hor,w
-	xorlw		0			; N„o deixa passar de 9
+	xorlw		0			; N√£o deixa passar de 9
 	btfss		STATUS,Z
 	decf		u_hor,f
 	; Decremento na dezena de minutos
 	movf		incre,w
 	xorlw		3
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_min,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 0 em dezenas de minutos ( Caso acontecesse o display apagava)
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 0 em dezenas de minutos ( Caso acontecesse o display apagava)
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		d_min,f
 	; Decremento na unidade de minutos
 	movf		incre,w
 	xorlw		4
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		u_min,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 9 unidade dm minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 9 unidade dm minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		u_min,f
 	
 
@@ -680,15 +680,15 @@ ajustarAlarme
 		; Incrementa na dezena de horas
 	movf		incre,w
 	xorlw		5
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		@4			
 	btfsc		PORTB,2		
 	goto		@4
 	btfss		PORTB,2
 	goto		$-1
 	movf		d_alarmeH,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$+2
 	goto		@4
 	movf		u_alarmeH,w
@@ -713,111 +713,111 @@ ajustarAlarme
 	; Incrementa na unidade de horas
 @4	movf		incre,w
 	xorlw		6
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+17			
 	btfsc		PORTB,2		
 	goto		$+15
 	btfss		PORTB,2
 	goto		$-1
 	movf		d_alarmeH,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$ + 5
 	movf		u_alarmeH,w
 	xorlw		3			; Se a unidade de hora for 3 ele vai pro goto que zera
 	btfsc		STATUS,Z
 	goto		$ + 5
 	movf		u_alarmeH,w
-	xorlw		9			; N„o deixa passar de 9
+	xorlw		9			; N√£o deixa passar de 9
 	btfss		STATUS,Z
 	incf		u_alarmeH,f
 	
 	; Incrementa na dezena de minutos
 	movf		incre,w
 	xorlw		7
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,2		
 	goto		$+7
 	btfss		PORTB,2
 	goto		$-1
 	movf		d_alarmeM,w		
-	xorlw		5			; Faz o teste para n„o exceder o valor 9 em dezenas de minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		5			; Faz o teste para n√£o exceder o valor 9 em dezenas de minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	incf		d_alarmeM,f
 
 	; Incrementa na unidade de minutos
 	movf		incre,w
 	xorlw		8
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,2		
 	goto		$+7
 	btfss		PORTB,2
 	goto		$-1
 	movf		u_alarmeM,w		
-	xorlw		9			; Faz o teste para n„o exceder o valor 9 unidade dm minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		9			; Faz o teste para n√£o exceder o valor 9 unidade dm minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	incf		u_alarmeM,f
 	; Decremento na dezena de horas
 	movf		incre,w
 	xorlw		5
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_alarmeH,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 0 unidade de hora
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 0 unidade de hora
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		d_alarmeH,f
 	; Decremento na unidade de horas
 	movf		incre,w
 	xorlw		6
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+17			
 	btfsc		PORTB,1		
 	goto		$+15
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_alarmeH,w		
-	xorlw		2			; Faz o teste para n„o exceder o valor 2 em horas
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		2			; Faz o teste para n√£o exceder o valor 2 em horas
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	goto		$ + 5
 	movf		u_alarmeH,w
 	xorlw		0			; Se a unidade de hora for diferente 0 ele vai pro goto que zera
 	btfsc		STATUS,Z
 	goto		$ + 5
 	movf		u_alarmeH,w
-	xorlw		0			; N„o deixa passar de 0
+	xorlw		0			; N√£o deixa passar de 0
 	btfss		STATUS,Z
 	decf		u_alarmeH,f
 	; Decremento na dezena de minutos
 	movf		incre,w
 	xorlw		7
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		d_alarmeM,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 0 em dezenas de minutos ( Caso acontecesse o display apagava)
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 0 em dezenas de minutos ( Caso acontecesse o display apagava)
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		d_alarmeM,f
 	; Decremento na unidade de minutos
 	movf		incre,w
 	xorlw		8
-	btfss		STATUS,Z	; Se for Z=0, pula e vai para o prÛximo teste
+	btfss		STATUS,Z	; Se for Z=0, pula e vai para o pr√≥ximo teste
 	goto		$+9			
 	btfsc		PORTB,1		
 	goto		$+7
 	btfss		PORTB,1
 	goto		$-1
 	movf		u_alarmeM,w		
-	xorlw		0			; Faz o teste para n„o exceder o valor 9 unidade dm minutos
-	btfss		STATUS,Z	; Z=1 o cÛdigo pula
+	xorlw		0			; Faz o teste para n√£o exceder o valor 9 unidade dm minutos
+	btfss		STATUS,Z	; Z=1 o c√≥digo pula
 	decf		u_alarmeM,f
 	
 
@@ -829,7 +829,7 @@ alarme
 	
 	movf	aux,w
 	xorlw	00000000b	;Caso a variavel do alarme estiver zerada
-	btfss	STATUS,Z	; pula pro fim do cÛdigo
+	btfss	STATUS,Z	; pula pro fim do c√≥digo
 	goto	$+5
 	bcf		PORTB,6
 	clrf	toca
